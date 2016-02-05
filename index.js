5a51571f95c8dc6d5b9583de95e537b57224c208
Naod Yeheyes <naod@blencorp.com>
2016-02-05 16:50:40 -0500

    Included cldr library and Added functions.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-exports.printMsg = function () {
-  console.log('Module works');
-}
+var caethiopic = require('./bin/cldr-cal-ethiopic-full/main/am/ca-ethiopic');
+var etmonths = caethiopic.main.am.dates.calendars.ethiopic.months;
+var etdays = caethiopic.main.am.dates.calendars.ethiopic.days;
+
+module.exports = {
+  month: function(month, type) {
+    if(!type) type = 'wide';
+    return etmonths.format[type][month];
+  },
+  day: function(day, type) {
+    if(!type) type = 'wide';
+    return etdays.format[type][day];
+  },
+};
 
 
 
