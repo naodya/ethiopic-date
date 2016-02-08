db59e0966fc556c2c6510c5f1ea1ffb15dd4bea0
Naod Yeheyes <naod@blencorp.com>
2016-02-08 00:51:25 -0500

    Fixed date conversion bug.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var caethiopic = require('./lib/cldr-cal-ethiopic-full/main/am/ca-ethiopic');
 var moment = require('moment-timezone');
 
-
-
 function EtCalendar() {
 
   var etmonths = caethiopic.main.am.dates.calendars.ethiopic.months;
@@ -10,67 +8,101 @@ function EtCalendar() {
   var dayperiods = caethiopic.main.am.dates.calendars.ethiopic.dayPeriods;
   moment.tz.setDefault('Africa/Addis_Ababa');
 
-
   var calendar = {
-    offset: 79372, //number of days between 1/1/1970 (UNIX time) and the calendar beginning year in this case 1753
-    daymillisecond: 1000*60*60*24, //1000*60*60*24 represents one day in milliseconds.
-    unixtimestamp: Date.parse(moment().format('M d, YYYY')),
-    addistime: moment().format('MMMM d, YYYY h:mm A'),
+    // Number of days between 1/1/1970 (UNIX time) and the calendar beginning year in this case 1753.
+    offset: 79372,
+    // One day milliseconds.
+    daymillisecond: 1000*60*60*24,
   };
 
-  calendar.daysdelta = parseInt((calendar.unixtimestamp/calendar.daymillisecond) + calendar.offset);
+  var convertDate = function(gdate) {
 
-  calendar.month = function(month, format_type) {
-    if(!format_type) format_type = 'wide';
-    return etmonths.format[format_type][month];
+    var originalDate = gdate;
+    var addistime = gdate.format();
+    var ehour = gdate.format('h');
+    var eminute = gdate.format('mm');
+    var esecond = gdate.format('ss');
+    var eperiod = gdate.format('a');
+    var edayname = gdate.format('ddd');
+
+    // UTC timestamp minutes.
+    var timeStampMinute = gdate.utc().format('X')*1000
+
+
+    var days = parseInt((timeStampMinute/calendar.daymillisecond) + calendar.offset);
+    var check = 0;
+    // Ethiopian calendary year equivalent of the beginng year 1753.
+    var eyear = 1745;
+
+    // Check leap year.
+    while (check == 0){
+      if (eyear % 4 == 3)	{
+        if (days >= 366) {
+          days -= 366;
+          eyear++;
+        } else {check = 1;}
+      } else {
+        if (days >= 365) {
+          days -= 365;
+          eyear++;
+        } else {check = 1;}
+      }
+    }
+
+    if (parseInt(days) == 0) {
+      eyear -= 1;
+      emonth = 13;
+      eday = 5 + ((eyear % 4 == 3) ? 1 : 0);
+    } else {
+      emonth = Math.ceil(days / 30);
+      if (days % 30 == 0) {eday = 30;}
+      else {eday = days % 30;}
+    }
+
+    return {
+      'year': eyear,
+      'month': emonth,
+      'day': eday,
+      'dayname': edayname.toLowerCase(),
+      'hour': ehour,
+      'minute': eminute,
+      'second': esecond,
+      'period': eperiod,
+      'addistime': moment().format(),
+      'originaldate': originalDate.format()
+    }
+
   };
 
-  calendar.day = function(day, format_type) {
-    if(!format_type) format_type = 'wide';
-    return etdays.format[format_type][day];
+  calendar.month = function(month, formatType) {
+    if(!formatType) formatType = 'wide';
+    return etmonths.format[formatType][month];
   };
 
-  calendar.dayperiod = function(period, format_type) {
-    if(!format_type) format_type = 'wide';
-    return dayperiods.format[format_type][period];
+  calendar.day = function(day, formatType) {
+    if(!formatType) formatType = 'wide';
+    return etdays.format[formatType][day];
+  };
+
+  calendar.dayperiod = function(period, formatType) {
+    if(!formatType) formatType = 'wide';
+    return dayperiods.format[formatType][period];
   };
 
   calendar.now = function() {
-    var days = calendar.daysdelta;
-		var check = 0;
-		var eyear = 1745; //Ethiopian calendary year equivalent of the beginng year 1753
 
+    var converted =  convertDate(moment(moment().format()));
 
-		while (check == 0){
-      if (eyear % 4 == 3)	{
-				if (days >= 366) {
-					days -= 366;
-					eyear++;
-				} else {check = 1;}
-			} else {
-				if (days >= 365) {
-					days -= 365;
-					eyear++;
-				} else {check = 1;}
-			}
-		}
+    var result = calendar.day(converted.dayname) + ', ';
+    result += calendar.month(converted.month) + ' ';
+    result += converted.day + ', ';
+    result += converted.year + ' ';
+    result += converted.hour + ':';
+    result += converted.minute + ':';
+    result += converted.second + ' ';
+    result += calendar.dayperiod(converted.period);
 
-		if (parseInt(days) == 0) {
-      eyear -= 1;
-			emonth = 13;
-			eday = 5 + ((eyear % 4 == 3) ? 1 : 0);
-		} else {
-			emonth = Math.ceil(days / 30);
-			if (days % 30 == 0) {eday = 30;}
-			else {eday = days % 30;}
-
-		}
-
-    edayname = moment().format('ddd').toLowerCase();
-    etime = moment().format('h:mm');
-    period = moment().format('A').toLowerCase();
-
-    return calendar.day(edayname) + ', ' + calendar.month(emonth) + ' ' + eday + ', ' + eyear + ' ' + etime + ' ' + calendar.dayperiod(period);
+    return result;
 
   };
 
@@ -79,6 +111,3 @@ function EtCalendar() {
 }
 
 module.exports =  EtCalendar();
-
-
-
